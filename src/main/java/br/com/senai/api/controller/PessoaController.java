86b160472f2f1c4425c9ba2b911e90f528334ca1
Roberta Baseggio <roberta_polez@estudante.sc.senai.br>
2021-06-01 16:51:49 -0300

    Criação da classe de erros e refatoração do codigo

--- src/main/java/br/com/senai/api/controller/PessoaController.java
+++ src/main/java/br/com/senai/api/controller/PessoaController.java
@@ -2,6 +2,7 @@ package br.com.senai.api.controller;
 
 import br.com.senai.domain.model.Pessoa;
 import br.com.senai.domain.repository.PessoaRepository;
+import br.com.senai.domain.service.PessoaService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ import java.util.Optional;
 public class PessoaController {
 
     private PessoaRepository pessoaRepository;
+    private PessoaService pessoaService;
 
     @GetMapping
     public List<Pessoa> listar(){
@@ -54,7 +56,7 @@ public class PessoaController {
     @PostMapping
     public Pessoa cadastrar(@Valid @RequestBody Pessoa pessoa){
 
-        return pessoaRepository.save(pessoa);
+        return pessoaService.cadastrar(pessoa);
     }
 
     @PutMapping("/{pessoaId}")
